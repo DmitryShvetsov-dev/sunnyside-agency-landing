1f40b8193d4fba03f0e8d52ee1264754f034cbba
DmitryShvetsov-dev <mod3rate@gmail.com>
2022-12-15 20:47:58 +0400

    add menu on mobile version

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuButton() {
+  let menu = document.getElementById("navigationBox");
+  menu.style.display = "flex";
+}
+function clearMenu() {
+  let menu = document.getElementById("navigationBox");
+  menu.style.display = "none";
+}
